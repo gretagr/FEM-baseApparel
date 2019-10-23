8d4476cc9fec7963eac43680f43c58f4447fb468
gretagr <greta.grigelionyte@gmail.com>
2019-10-23 21:28:23 +0300

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-let email = document.querySelector("form").value;
+document.getElementById('submit').addEventListener('click', () => {
+
+  let text = document.getElementById('email').value;
+
+  const icon = document.getElementById('icon');
+  const message = document.getElementById('err_message')
+  const input = document.getElementById('email');
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!regex.test(text)) {
+    input.classList.add('error');
+    icon.classList.add('error-icon');
+    message.classList.add('err');
+    console.log(text);
+  } else {
+    input.classList.remove('error');
+    icon.classList.remove('error-icon');
+    message.classList.remove('err');
+
+  }
+});
